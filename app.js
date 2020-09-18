7c0150c0af9821f05598bed7d145884a95c46304
Jacob W Anderson <37127146+jake22anderson@users.noreply.github.com>
2020-09-18 01:08:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 var client_id = '551ed56d58cb40a6ba0b5d8e9522de2f'; // Your client id
 var client_secret = 'edbc95099b8b464ea8d9880a86ca84b5'; // Your secret
 var redirect_uri = 'https://spotify-album-art.herokuapp.com/callback'; // Your redirect uri
+var scopes = \'user-read-private user-read-email\'
